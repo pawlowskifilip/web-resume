fa2bb5a5ce4db3558e5339b4360a2f200ed81b4b
pawlowsfilip <pawlows.filip@gmail.com>
2024-06-24 19:57:35 +0200

    added mobile menu

--- src/main.js
+++ src/main.js
@@ -10,16 +10,24 @@ window.addEventListener("scroll", () => {
 
 let openBtn = document.querySelector(".bi-list");
 let closeBtn = document.querySelector(".bi-x-lg");
-let menu = document.querySelector(".navbar__menu");
+let menu = document.querySelector(".navbar__menu--mobile");
 
 openBtn.addEventListener("click", ()=>{
     openBtn.classList.toggle("hide");
     closeBtn.classList.toggle("hide");
     menu.classList.toggle("hidden");
+    if (window.pageYOffset > 60) {
+        navbar.classList.remove("scrolled");
+    }
+    document.body.classList.toggle("stop-scrolling");
 });
 
 closeBtn.addEventListener("click", ()=>{
     closeBtn.classList.toggle("hide");
     openBtn.classList.toggle("hide");
     menu.classList.toggle("hidden");
+    if (window.pageYOffset > 60) {
+        navbar.classList.add("scrolled");
+    }
+    document.body.classList.toggle("stop-scrolling");
 });
